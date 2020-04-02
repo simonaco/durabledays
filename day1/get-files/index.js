c0f8384dfb7ccc2361ef44072c25f062acb3193f
Simona Cotin <simona.cotin@gmail.com>
2020-04-02 12:07:54 +0100

    first commit

---
+++ day1/get-files/index.js
@@ -0,0 +1,11 @@
+﻿const readdirp = require("readdirp");
+module.exports = async function(context, rootDirectory) {
+  context.log(`Searching for files under ${rootDirectory}...`);
+  const allFilePaths = [];
+  for await (const entry of readdirp(rootDirectory)) {
+    const { path } = entry;
+    console.log(`${JSON.stringify({ path })}`);
+    allFilePaths.push(entry.fullPath);
+  }
+  return allFilePaths;
+};

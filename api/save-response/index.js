3359248f188b039a8ac4e9e65619cf37816a3252
Simona Cotin <simona.cotin@gmail.com>
2020-04-15 14:09:27 +0100

    save state in table

---
+++ api/save-response/index.js
@@ -0,0 +1,13 @@
+﻿module.exports = async function (context, input) {
+  context.bindings.schedulerTable = [];
+  context.bindings.schedulerTable.push({
+    PartitionKey: "URL",
+    RowKey: input.instanceId,
+    Response: JSON.stringify(input.response),
+    URL: input.url,
+    Payload: input.payload,
+    ScheduledDate: input.scheduledDate,
+  });
+
+  return `Hello ${context.bindings.name}!`;
+};

4b43bf45ccbd005a884dd731621b2854653f145c
Simona Cotin <simona.cotin@gmail.com>
2020-04-15 15:51:40 +0100

    test deploy

--- api/get-status/index.js
+++ api/get-status/index.js
@@ -1,5 +1,5 @@
 module.exports = async function (context, req) {
-  context.log("Deploy from Github Action");
+  context.log("Deploy to specific path");
   const entries = context.bindings.schedulerTable;
 
   context.res = {
